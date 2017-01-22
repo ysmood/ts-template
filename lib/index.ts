2f0ef7f760dc3ff773911f9118d6cd26d7895b38
Yad Smood <y.s.inside@gmail.com>
2017-01-22 19:55:03 +0800

    up

---
+++ lib/index.ts
@@ -0,0 +1,12 @@
+
+// It's a linked list
+export interface Context {
+    parent: Context
+}
+
+function traverse (forest: any[], ctx: Context) {
+
+}
+
+
+export default traverse
---
+++ readme.md
@@ -0,0 +1,1 @@
+Traverse through multiple trees simultaneously via the first tree's path.
